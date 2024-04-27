8324b0b907d8881852d1371f965487f8599b83dd
ibrahim <ibrahimazeem41@gmail.com>
2024-04-28 02:27:16 +0500

    added useful action in worker controller

--- app/controllers/workers_controller.rb
+++ app/controllers/workers_controller.rb
@@ -1,2 +1,31 @@
 class WorkersController < ApplicationController
-end
+    before_action :set_worker, only: [:show, :update, :destroy]
+  
+    def index
+      @workers = Worker.all
+      render json: @workers
+    end
+  
+    def show
+      render json: @worker
+    end
+  
+    def create
+      @worker = Worker.new(worker_params)
+      if @worker.save
+        render json: @worker, status: :created
+      else
+        render json: @worker.errors, status: :unprocessable_entity
+      end
+    end
+  
+    private
+  
+    def set_worker
+      @worker = Worker.find(params[:id])
+    end
+  
+    def worker_params
+      params.require(:worker).permit(:name)
+    end
+end  
