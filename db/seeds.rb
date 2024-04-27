fcb98ae8e6a78dfe1ed26a36a0765807a99e6eb3
ibrahim <ibrahimazeem41@gmail.com>
2024-04-28 03:14:38 +0500

    added seeds file for worker and their shifts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# Create Workers
+7.times do |i|
+    Worker.create(name: FFaker::Name.name)
+end
+  
+# Create Shifts
+start_dates = [Date.today, Date.tomorrow, Date.today + 2.days]
+workers = Worker.all
+
+start_dates.each do |start_date|
+    workers.each do |worker|
+        Shift.create(
+        start_time: start_date.to_time + 8.hours, # Assuming the first shift starts at 8 AM
+        end_time: start_date.to_time + 16.hours,  # Assuming the first shift ends at 4 PM
+        date: start_date,
+        worker_id: worker.id
+        )
+    end
+end
